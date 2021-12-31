10986a8bebb1ff6730b2847beba079ecb59ea6f8
Rahmat <oktrifianto@gmail.com>
2021-12-31 22:21:10 +0700

    Add changeToggle Function
    There is a function call ID with textContent

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+function changeToggle(){
+  const ToggleButton  = document.getElementById("toggle");
+  const ButtonContent = ToggleButton.textContent;
+
+  ButtonContent == "Switch To Dark" ?
+    ToggleButton.textContent = "Switch To Light" : '';
+  
+  ButtonContent == "Switch To Light" ?
+    ToggleButton.textContent = "Switch To Dark" : '';
+}
